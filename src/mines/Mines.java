5c799bfbef9fe1d63da87f8391c778b76e5cd772
Safa BENABDESSADOK <BENABDESSADOK.safa@gmail.com>
2023-05-06 15:41:42 +0100

    Nous avons également supprimé le paramètre explicite de chaîne vide dans le constructeur JLabel, car une chaîne vide est la valeur par défaut pour le texte de l'étiquette. et la remplacement de JFrame.EXIT_ON_CLOSE par WindowConstants.EXIT_ON_CLOSE évite d'utiliser l'accession de membre statique sur une instance d'objet et rend le code plus lisible et maintenable

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -4,25 +4,29 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 
 // supprimé le commentaire indiquant la source du code 
 
 public class Mines extends JFrame {
     private static final long serialVersionUID = 4772165125287256837L;
 
-    private final int WIDTH = 250;
-    private final int HEIGHT = 290;
+    // permet un accès direct à ces valeurs
+    private static final int WIDTH = 250;
+    private static final int HEIGHT = 290;
 
     private JLabel statusbar;
 
     public Mines() {
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Remplacement de la ligne setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); par
+        // setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setTitle("Minesweeper");
 
-        statusbar = new JLabel("");
+        // supprition de le paramètre explicite de chaîne vide
+        statusbar = new JLabel();
         add(statusbar, BorderLayout.SOUTH);
 
         add(new Board(statusbar));
