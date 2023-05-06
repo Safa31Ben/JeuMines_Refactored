9e3a16b22452367c4e7cc495428289e9ca9314bf
Safa BENABDESSADOK <BENABDESSADOK.safa@gmail.com>
2023-05-06 15:25:29 +0100

    Nous avons supprimé le commentaire indiquant la source du code car il n'est pas nécessaire pour le fonctionnement du programme.

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -5,16 +5,16 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-// Source: http://zetcode.com/tutorials/javagamestutorial/minesweeper/
+// supprimé le commentaire indiquant la source du code 
 
 public class Mines extends JFrame {
-	private static final long serialVersionUID = 4772165125287256837L;
-	
-	private final int WIDTH = 250;
+    private static final long serialVersionUID = 4772165125287256837L;
+
+    private final int WIDTH = 250;
     private final int HEIGHT = 290;
 
     private JLabel statusbar;
-    
+
     public Mines() {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,7 +30,7 @@ public class Mines extends JFrame {
         setResizable(false);
         setVisible(true);
     }
-    
+
     public static void main(String[] args) {
         new Mines();
     }
